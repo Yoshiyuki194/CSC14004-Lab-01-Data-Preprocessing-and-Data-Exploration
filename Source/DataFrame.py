ee35b354faed500e990af3377580b70f0b873954
Nguyen Hoang Anh <76523655+Yoshiyuki194@users.noreply.github.com>
2023-03-24 14:54:56 +0700

    update

--- Source/DataFrame.py
+++ Source/DataFrame.py
@@ -6,6 +6,7 @@ class DataFrame():
         self.columns = columns
         self.values = values
 
+
     @staticmethod
     def read_csv(filename):
         with open(filename, "r") as file:
@@ -16,8 +17,24 @@ class DataFrame():
             for row in reader:
                 for column in columns:
                     values[column].append(row[column])
-
+                
         return DataFrame(columns, values)
+
+    def to_csv(self, filename):
+        header = self.columns
+        lines = []
+        lines.append(header)
+        
+        for idx, val in enumerate(self.values[header[0]]):
+            row = []
+            row.append(val)
+            for other in header[1: len(header)]:
+                row.append(self.values[other][idx])
+            lines.append(row)
+
+        with open(filename, "w") as file:
+            file.write(str(lines))
+        
 
     def __getitem__(self, attr: str):
         return self.values[attr]
@@ -29,5 +46,8 @@ class DataFrame():
 # dir_path = path.dirname(script_path)
 # input_dir = path.join(dir_path, 'House_Prices')
 # input_path = path.join(input_dir, 'test.csv')
+# output_path = path.join(input_dir, 'test_out.csv')
 # df = DataFrame.read_csv(input_path)
-# print(df['LotArea'][:5])
+# df.to_csv(output_path)
+# print(df['OverallQual'][:5])
+# print(df.dtypes['OverallQual'])
