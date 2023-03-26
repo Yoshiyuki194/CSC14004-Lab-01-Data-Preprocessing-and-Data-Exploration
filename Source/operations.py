558e4b83475a7e052e2cd056d9472c2a737750d4
Nguyen Hoang Anh <76523655+Yoshiyuki194@users.noreply.github.com>
2023-03-26 12:13:51 +0700

    final commit

--- Source/operations.py
+++ Source/operations.py
@@ -4,7 +4,7 @@ import DataFrame as df
 
 parser = arg.ArgumentParser()
 parser.add_argument('input_path', type = str, help = "Path to the input file")
-parser.add_argument('--opt', type = chr, help = "Operator to perform")
+parser.add_argument('--opt', type = str, help = "Operator to perform")
 parser.add_argument('--column1', type = str, help = "Name of the column as the first operand")
 parser.add_argument('--column2', type = str, help = "Name of the column as the second operand")
 parser.add_argument('--res', type = str, help = "Name of the result column")
@@ -12,19 +12,19 @@ args = parser.parse_args()
 
 data = df.DataFrame.read_csv(args.input_path)
 # Verify both columns have numeric data type
-if data.type(arg.column1) != 'Numeric' or data.type(arg.column2) != 'Numeric':
-    arg.ArgumentTypeError('Invalid pair of columns. Both must have numeric data')
+if data.type(args.column1) != 'Numeric' or data.type(args.column2) != 'Numeric':
+   arg.ArgumentTypeError('Invalid pair of columns. Both must have numeric data')
 # Parsing
-col1 = data[arg.column1]
-col2 = data[arg.column2]
-tmp = zip(col1, col2)
+tmp = zip(data[args.column1], data[args.column2])
 if args.opt == '+':
-    data[arg.res] = [i + j for i, j in tmp]
-elif args.out == '-':
-    data[arg.res] = [i - j for i, j in tmp]
-elif args.out == '*':
-    data[arg.res] = [i * j for i, j in tmp]
-elif args.out == '/':
-    data[arg.res] = [i / j for i, j in tmp]
+    data[args.res] = [float(i) + float(j) if i != '' and j != '' else '' for i, j in tmp]
+elif args.opt == '-':
+    data[args.res] = [float(i) - float(j) if i != '' and j != '' else '' for i, j in tmp]
+elif args.opt == '*':
+    data[args.res] = [float(i) * float(j) if i != '' and j != '' else '' for i, j in tmp]
+elif args.opt == '/':
+    data[args.res] = [float(i) / float(j) if i != '' and j != '' else '' for i, j in tmp]
 else:
     arg.ArgumentTypeError('Invalid operators')
+data.columns.append(args.res)
+data.to_csv(args.input_path)
